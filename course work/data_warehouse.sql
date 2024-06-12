1cce2d09720694f1be3632ec76f53e904d435418
TrukshaninaAnna <trukshanina.anna@student.ehu.lt>
2024-06-12 20:41:21 +0300

    Update data_warehouse.sql

--- course work/data_warehouse.sql
+++ course work/data_warehouse.sql
@@ -1,14 +1,13 @@
--- Создание схемы для хранилища данных
 CREATE SCHEMA datawarehouse;
 
 
--- Таблица измерений категорий
+-- DimCategory table
 CREATE TABLE IF NOT EXISTS datawarehouse.DimCategories (
     CategoryID INT PRIMARY KEY,
     CategoryName VARCHAR(100) NOT NULL
 );
 
--- Таблица измерений для дат
+-- DimDate table
 CREATE TABLE datawarehouse.DimDate (
     DateID SERIAL PRIMARY KEY,
     Date DATE NOT NULL,
@@ -19,7 +18,36 @@ CREATE TABLE datawarehouse.DimDate (
 	CONSTRAINT unique_date UNIQUE (Date)
 );
 
--- Таблица измерений для продуктов
+-- DimUsers table
+CREATE TABLE datawarehouse.DimUsers (
+    UserID INT PRIMARY KEY,
+    Username VARCHAR(100) NOT NULL,
+    Email VARCHAR(100),
+    Address VARCHAR(255),
+    Phone VARCHAR(20)
+);
+
+-- Dimantion Country of Origin table
+CREATE TABLE datawarehouse.DimCountries (
+    CountryID INT PRIMARY KEY,
+    CountryName VARCHAR(100) NOT NULL
+);
+
+-- DimManufacturers table
+CREATE TABLE datawarehouse.DimManufacturers (
+    ManufacturerID INT PRIMARY KEY,
+    ManufacturerName VARCHAR(100) NOT NULL,
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES datawarehouse.DimCountries(CountryID)
+);
+
+-- DimBrands table
+CREATE TABLE datawarehouse.DimBrands (
+    BrandID INT PRIMARY KEY,
+    BrandName VARCHAR(100) NOT NULL
+); 
+
+-- DimProducts
 CREATE TABLE datawarehouse.DimProducts (
     ProductID INT PRIMARY KEY,
     ProductName VARCHAR(255) NOT NULL,
@@ -33,35 +61,7 @@ CREATE TABLE datawarehouse.DimProducts (
     FOREIGN KEY (ManufacturerID) REFERENCES datawarehouse.DimManufacturers(ManufacturerID)
 );
 
--- Таблица измерений для пользователей
-CREATE TABLE datawarehouse.DimUsers (
-    UserID INT PRIMARY KEY,
-    Username VARCHAR(100) NOT NULL,
-    Email VARCHAR(100),
-    Address VARCHAR(255),
-    Phone VARCHAR(20)
-);
-
--- Таблица измерений для производителей
-CREATE TABLE datawarehouse.DimManufacturers (
-    ManufacturerID INT PRIMARY KEY,
-    ManufacturerName VARCHAR(100) NOT NULL,
-    CountryID INT
-);
-
--- Таблица измерений для стран
-CREATE TABLE datawarehouse.DimCountries (
-    CountryID INT PRIMARY KEY,
-    CountryName VARCHAR(100) NOT NULL
-);
-
--- Таблица измерений для брендов
-CREATE TABLE datawarehouse.DimBrands (
-    BrandID INT PRIMARY KEY,
-    BrandName VARCHAR(100) NOT NULL
-);
-
--- Таблица фактов для продаж
+-- Facts table for sales
 CREATE TABLE datawarehouse.Sales_Fact (
     SalesID SERIAL PRIMARY KEY,
     OrderID INT,
@@ -77,17 +77,8 @@ CREATE TABLE datawarehouse.Sales_Fact (
     FOREIGN KEY (DateID) REFERENCES datawarehouse.DimDate(DateID)
 );
 
--- Таблица фактов для связи производителей и стран
-CREATE TABLE datawarehouse.ManufacturerCountry_Fact (
-    ManufacturerCountryID SERIAL PRIMARY KEY,
-    ManufacturerID INT,
-    CountryID INT,
-    FOREIGN KEY (ManufacturerID) REFERENCES datawarehouse.DimManufacturers(ManufacturerID),
-    FOREIGN KEY (CountryID) REFERENCES datawarehouse.DimCountries(CountryID)
-);
 
-
--- Таблица фактов для рейтингов продуктов
+-- Facts table for rating
 CREATE TABLE datawarehouse.ProductRatings_Fact (
     RatingID SERIAL PRIMARY KEY,
     ProductID INT,
@@ -101,44 +92,124 @@ CREATE TABLE datawarehouse.ProductRatings_Fact (
 );
 
 
--- Добавление новых столбцов для поддержки SCD Type 2
 ALTER TABLE datawarehouse.DimProducts
 ADD COLUMN StartDate DATE DEFAULT CURRENT_DATE,
 ADD COLUMN EndDate DATE DEFAULT '9999-12-31',
 ADD COLUMN IsCurrent BOOLEAN DEFAULT TRUE;
-
 ALTER TABLE datawarehouse.DimProducts
 ADD CONSTRAINT unique_product UNIQUE (ProductID, BrandID, CategoryID, ManufacturerID);
 
--- Обновление существующих записей, устанавливая текущую дату в качестве StartDate
-UPDATE datawarehouse.DimProducts
-SET StartDate = CURRENT_DATE;
 
--- Обновление существующих записей, устанавливая EndDate по умолчанию
-UPDATE datawarehouse.DimProducts
-SET EndDate = '9999-12-31';
-
--- Процедура обновления DimProducts для поддержки SCD Type 2
-CREATE OR REPLACE FUNCTION update_dim_products(
-    p_product_id INT,
-    p_price NUMERIC(10, 2),
-    p_availability_status BOOLEAN
-) RETURNS VOID AS $$
+--SCD2
+CREATE OR REPLACE FUNCTION dim_products_update_trigger()
+RETURNS TRIGGER AS $$
 BEGIN
-    -- Обновление текущих записей, устанавливая EndDate и IsCurrent = FALSE
-    UPDATE datawarehouse.DimProducts
-    SET EndDate = CURRENT_DATE - INTERVAL '1 day', IsCurrent = FALSE
-    WHERE ProductID = p_product_id AND IsCurrent = TRUE;
-    
-    -- Вставка новой записи
-    INSERT INTO datawarehouse.DimProducts (ProductID, ProductName, BrandID, CategoryID, ManufacturerID, Price, AvailabilityStatus, StartDate, EndDate, IsCurrent)
-    SELECT ProductID, ProductName, BrandID, CategoryID, ManufacturerID, p_price, p_availability_status, CURRENT_DATE, '9999-12-31', TRUE
-    FROM datawarehouse.DimProducts
-    WHERE ProductID = p_product_id
-    ORDER BY StartDate DESC
-    LIMIT 1;
+    -- Проверяем, изменилось ли одно из значимых полей и установлен ли флаг текущей записи в TRUE
+    IF (OLD.ProductName <> NEW.ProductName 
+        OR OLD.CategoryID <> NEW.CategoryID 
+        OR OLD.BrandID <> NEW.BrandID 
+        OR OLD.ManufacturerID <> NEW.ManufacturerID 
+        OR OLD.Price <> NEW.Price 
+        OR OLD.AvailabilityStatus <> NEW.AvailabilityStatus)
+        AND OLD.IsCurrent THEN
+        
+        -- Обновляем текущую запись, устанавливая дату окончания и флаг текущей записи в FALSE
+        UPDATE datawarehouse.DimProducts
+        SET EndDate = CURRENT_DATE,
+            IsCurrent = FALSE
+        WHERE ProductID = OLD.ProductID AND IsCurrent = TRUE;
+
+        -- Вставляем новую запись с новыми значениями и текущей датой в качестве даты начала
+        INSERT INTO datawarehouse.DimProducts (ProductID, ProductName, CategoryID, BrandID, ManufacturerID, Price, AvailabilityStatus, StartDate, EndDate, IsCurrent)
+        VALUES (OLD.ProductID, NEW.ProductName, NEW.CategoryID, NEW.BrandID, NEW.ManufacturerID, NEW.Price, NEW.AvailabilityStatus, CURRENT_DATE, '9999-12-31', TRUE);
+    END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+
+-- Создаем триггер для таблицы DimProducts, который будет срабатывать после обновления
+CREATE TRIGGER dim_products_update
+AFTER UPDATE ON datawarehouse.DimProducts
+FOR EACH ROW
+EXECUTE FUNCTION dim_products_update_trigger();
+
+
+
+CREATE VIEW datawarehouse.sales_trend AS
+SELECT 
+    dd.Date,
+    SUM(sf.TotalCost) AS TotalSales
+FROM datawarehouse.Sales_Fact sf
+JOIN datawarehouse.DimDate dd ON sf.DateID = dd.DateID
+GROUP BY dd.Date
+ORDER BY dd.Date;
+
+
+CREATE VIEW datawarehouse.sales_by_user AS
+SELECT 
+    du.UserID,
+    du.Username,
+    COUNT(sf.SalesID) AS NumberOfSales,
+    SUM(sf.TotalCost) AS TotalSales
+FROM datawarehouse.Sales_Fact sf
+JOIN datawarehouse.DimUsers du ON sf.UserID = du.UserID
+GROUP BY du.UserID, du.Username
+ORDER BY du.UserID;
+	
+	
+CREATE VIEW datawarehouse.sales_by_country_of_origin AS
+SELECT 
+    dc.CountryName,
+    SUM(sf.TotalCost) AS TotalSales
+FROM datawarehouse.Sales_Fact sf
+JOIN datawarehouse.DimProducts dp ON sf.ProductID = dp.ProductID
+JOIN datawarehouse.DimManufacturers mcf ON dp.ManufacturerID = mcf.ManufacturerID
+JOIN datawarehouse.DimCountries dc ON mcf.CountryID = dc.CountryID
+GROUP BY dc.CountryName
+ORDER BY TotalSales DESC;
+
+
+CREATE VIEW datawarehouse.sales_by_category AS
+SELECT 
+    dcat.CategoryName,
+    SUM(sf.TotalCost) AS TotalSales
+FROM datawarehouse.Sales_Fact sf
+JOIN datawarehouse.DimProducts dp ON sf.ProductID = dp.ProductID
+JOIN datawarehouse.DimCategories dcat ON dp.CategoryID = dcat.CategoryID
+GROUP BY dcat.CategoryName
+ORDER BY TotalSales DESC;
+
+
+CREATE VIEW datawarehouse.sales_by_brand AS
+SELECT 
+    db.BrandName,
+    SUM(sf.TotalCost) AS TotalSales
+FROM datawarehouse.Sales_Fact sf
+JOIN datawarehouse.DimProducts dp ON sf.ProductID = dp.ProductID
+JOIN datawarehouse.DimBrands db ON dp.BrandID = db.BrandID
+GROUP BY db.BrandName
+ORDER BY TotalSales DESC;
+
+
+CREATE VIEW datawarehouse.average_rating_by_product AS
+SELECT 
+    dp.ProductName,
+    AVG(prf.Rating) AS AverageRating
+FROM datawarehouse.ProductRatings_Fact prf
+JOIN datawarehouse.DimProducts dp ON prf.ProductID = dp.ProductID
+GROUP BY dp.ProductName
+ORDER BY AverageRating DESC;
+
+
+CREATE VIEW datawarehouse.average_rating_by_brand AS
+SELECT 
+    db.BrandName,
+    AVG(prf.Rating) AS AverageRating
+FROM datawarehouse.ProductRatings_Fact prf
+JOIN datawarehouse.DimProducts dp ON prf.ProductID = dp.ProductID
+JOIN datawarehouse.DimBrands db ON dp.BrandID = db.BrandID
+GROUP BY db.BrandName
+ORDER BY AverageRating DESC;
 
 
